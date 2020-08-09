1f3a4aa1c96c028b80ba6b4fac931334bf2ec803
Nithin Reddy <58132052+nithinreddy981@users.noreply.github.com>
2020-08-09 11:48:36 +0530

    Update makeCacheMatrix.R

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,5 +1,10 @@
+##  it is used for Caching the Inverse of a Matrix
+## Matrix inversion is usually a costly computation  
+## it is used to catch inverse rather than doing samething repeatedly 
+##  it stores a matrix
+## This function take inverse of a matrix 
 makeCacheMatrix<-function(x=matrix()){
-  inv<-NULL,
+  inv<-NULL
   set<-function(y){
     x<<-y
     inv<-NULL
@@ -11,6 +16,7 @@ makeCacheMatrix<-function(x=matrix()){
 }
 
 cacheSolve<-function(x,...){
+  ##  it returns  a matrix  inverse of 'x'
   inv<-x$getInverse()
   if(!is.null(inv)){
     message("getting cached data")
